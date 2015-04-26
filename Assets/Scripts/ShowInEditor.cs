c30455ac6745d6e3e304641cab1489b1524d6c2d
Kevin "Gaeel" Bradshaw-Rodriguez <gaeel@nanoleptic.net>
2015-04-26 15:26:18 +0200

    Adds spawn points to pitch prefab

---
+++ Assets/Scripts/ShowInEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowInEditor : MonoBehaviour {
+
+    public Color color;
+    public float size;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawSphere(transform.position, size);
+    }
+
+}
