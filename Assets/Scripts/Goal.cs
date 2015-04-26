5d826997e94ce76f47369c950eb381135a79f396
Kevin "Gaeel" Bradshaw-Rodriguez <gaeel@nanoleptic.net>
2015-04-26 20:13:08 +0200

    Add goal script to goal triggers

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+    public Team side;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag=="Ball")
+        {
+            var shooter = Movement.BallGrabber.BallOwner.GetComponent<PlayerCharacter>();
+            if (shooter!=null)
+                GameEvent.RaiseOnGoal(shooter, side);
+        }
+    }
+
+
+}
