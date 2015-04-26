509fc24646f30ef49fb551eb1a76d20c71346c3e
Kevin "Gaeel" Bradshaw-Rodriguez <gaeel@nanoleptic.net>
2015-04-26 17:42:02 +0200

    Adds Pitch Zones

--- Assets/Scripts/PitchZone.cs
+++ Assets/Scripts/PitchZone.cs
@@ -4,6 +4,7 @@ using System.Collections;
 [RequireComponent(typeof(Collider2D))]
 public class PitchZone : MonoBehaviour {
 
+
     Collider2D _col;
 
     public void Start()
@@ -26,12 +27,29 @@ public class PitchZone : MonoBehaviour {
 
     void OnTriggerExit2D(Collider2D col)
     {
+        switch (col.tag)
+        {
+            case "Ball":
+                GameEvent.RaiseOnBallZoneExit(_col);
+                break;
+            case "Player":
+                GameEvent.RaiseOnPlayerZoneExit(_col, col.GetComponent<PlayerCharacter>());
+                break;
+        }
 
     }
 
     void OnTriggerStay2D(Collider2D col)
     {
-
+        switch (col.tag)
+        {
+            case "Ball":
+                GameEvent.RaiseOnBallZoneExit(_col);
+                break;
+            case "Player":
+                GameEvent.RaiseOnPlayerZoneExit(_col, col.GetComponent<PlayerCharacter>());
+                break;
+        }
     }
 
 }
