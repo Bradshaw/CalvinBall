3ebee3ffc5040343e4873be6b8f4638ac16395fd
Kevin "Gaeel" Bradshaw-Rodriguez <gaeel@nanoleptic.net>
2015-04-26 19:09:24 +0200

    Fix git repo

--- Assets/Scripts/Rules/Rule.cs
+++ Assets/Scripts/Rules/Rule.cs
@@ -10,21 +10,10 @@ public enum RuleType
     constraint
 }
 
-<<<<<<< HEAD
 public class Rule : MonoBehaviour 
 { 
 	public override string ToString ()
 	{
 		return "NORULE|NOTYPE";
 	}
-
-=======
-public class Rule : MonoBehaviour {
-
-    public override string ToString()
-    {
-        return "NORULE";
-    }
->>>>>>> 848c614de2c11a54bbc5169d8f28268c7858eeed
-
 }
